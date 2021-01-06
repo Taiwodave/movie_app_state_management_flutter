05b12b48511d5900936b2ff991a01c250aaa7589
Andrea Bizzotto <bizz84@gmail.com>
2021-01-06 15:21:55 +0000

    Refactor ScrollableMoviesPageBuilder out of NowPlayingPage

--- apps/flutter_bloc/lib/app/now_playing_page.dart
+++ apps/flutter_bloc/lib/app/now_playing_page.dart
@@ -5,7 +5,9 @@ import 'package:tmdb_flutter_bloc_demo/blocs/movies/movies_cubit.dart';
 import 'package:tmdb_flutter_bloc_demo/blocs/movies/movies_state.dart';
 import 'package:core/ui/movies_grid.dart';
 
-class NowPlayingPage extends StatefulWidget {
+import 'package:core/ui/scrollable_movies_page_builder.dart';
+
+class NowPlayingPage extends StatelessWidget {
   static Widget create(BuildContext context) {
     return BlocProvider<MoviesCubit>(
       create: (_) => MoviesCubit(api: TMDBClient()),
@@ -14,70 +16,35 @@ class NowPlayingPage extends StatefulWidget {
   }
 
   @override
-  _NowPlayingPageState createState() => _NowPlayingPageState();
-}
-
-class _NowPlayingPageState extends State<NowPlayingPage> {
-  final _scrollController = ScrollController();
-  final _scrollThreshold = 200.0;
-
-  @override
-  void initState() {
-    super.initState();
-    _scrollController.addListener(_onScroll);
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  void _onScroll() {
-    final maxScroll = _scrollController.position.maxScrollExtent;
-    final currentScroll = _scrollController.position.pixels;
-    if (maxScroll - currentScroll <= _scrollThreshold) {
-      final moviesCubit = BlocProvider.of<MoviesCubit>(context);
-      moviesCubit.fetchNextPage();
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Now Playing'),
-      ),
-      body: buildContent(context),
-      backgroundColor: Colors.black,
-    );
-  }
-
-  Widget buildContent(BuildContext context) {
-    return BlocBuilder<MoviesCubit, MoviesState>(
-      builder: (context, state) {
-        return state.when(
-          data: (movies, _) => MoviesGrid(
-            movies: movies,
-            controller: _scrollController,
-          ),
-          dataLoading: (movies) {
-            return movies.isEmpty
-                ? _buildLoading()
-                : MoviesGrid(
-                    movies: movies,
-                    controller: _scrollController,
-                  );
+    return ScrollableMoviesPageBuilder(
+      onNextPageRequested: () {
+        final moviesCubit = BlocProvider.of<MoviesCubit>(context);
+        moviesCubit.fetchNextPage();
+      },
+      builder: (context, controller) {
+        return BlocBuilder<MoviesCubit, MoviesState>(
+          builder: (context, state) {
+            return state.when(
+              data: (movies, _) => MoviesGrid(
+                movies: movies,
+                controller: controller,
+              ),
+              dataLoading: (movies) {
+                return movies.isEmpty
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : MoviesGrid(
+                        movies: movies,
+                        controller: controller,
+                      );
+              },
+              error: (error) => Center(child: Text(error)),
+            );
           },
-          error: (error) => Center(child: Text(error)),
         );
       },
-    );
-  }
-
-  Widget _buildLoading() {
-    return const Center(
-      child: CircularProgressIndicator(),
     );
   }
 }
