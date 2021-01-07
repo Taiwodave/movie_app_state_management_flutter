0b64149f478670e5dbb3c972a766ad48bffe596c
Andrea Bizzotto <bizz84@gmail.com>
2021-01-07 09:59:31 +0000

    Use same project name for all apps on ios, android

--- apps/flutter_bloc/android/app/src/main/kotlin/com/example/movie_app_demo_flutter/MainActivity.kt
+++ apps/flutter_bloc/android/app/src/main/kotlin/com/example/movie_app_demo_flutter/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.movie_app_demo_riverpod
+package com.example.movie_app_demo_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
--- apps/riverpod/android/app/src/main/kotlin/com/example/movie_app_demo_flutter/MainActivity.kt
+++ apps/riverpod/android/app/src/main/kotlin/com/example/movie_app_demo_flutter/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.movie_app_demo_flutter_bloc
+package com.example.movie_app_demo_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
