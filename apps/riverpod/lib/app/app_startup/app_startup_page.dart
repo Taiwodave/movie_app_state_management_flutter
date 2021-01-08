475c81d232aba6e6aa8cefed825c605aba5b86a0
Andrea Bizzotto <bizz84@gmail.com>
2021-01-08 14:59:39 +0000

    Remove duplicate provider

--- apps/riverpod/lib/app/app_startup/app_startup_page.dart
+++ apps/riverpod/lib/app/app_startup/app_startup_page.dart
@@ -1,4 +1,3 @@
-import 'package:core/models/app_models/profiles_data.dart';
 import 'package:flutter/material.dart';
 import 'package:core/ui/home_navigation_builder.dart';
 
@@ -7,14 +6,6 @@ import 'package:movie_app_demo_flutter/app/favourites/favourites_page.dart';
 import 'package:movie_app_demo_flutter/app/now_playing/now_playing_page.dart';
 import 'package:movie_app_demo_flutter/app/profile_selection/profile_selection_page.dart';
 import 'package:movie_app_demo_flutter/top_level_providers.dart';
-
-final profilesDataProvider = Provider<ProfilesData>((ref) {
-  final state = ref.watch(appStartupModelProvider.state);
-  return state.when(
-      initializing: () => null,
-      needsProfile: () => ProfilesData(),
-      profileLoaded: (profilesData) => profilesData);
-});
 
 /// This is the "root" widget of the app, which sits just below MaterialApp.
 /// It performs app-state initialization and returns the appropriate page.
